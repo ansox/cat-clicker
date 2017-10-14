ce1c10ef358d4f8a7693314e73fad5ce0f9638c6
Anderson Floriano <andersonfloriano@MacBook-Pro-de-Anderson.local>
2017-10-14 00:58:30 -0300

    Changed to MVO

--- app.js
+++ app.js
@@ -1,79 +1,124 @@
-catList = [
-    {
-      name: 'Gato 1',
-      image: 'assets/images/cat.jpg',
-      counter: 0
-    }, 
-  
-    {
-      name: 'Gato 2',
-      image: 'assets/images/cat2.jpg',
-      counter: 0
+var model = {
+  init: function() {
+    this.catList = [
+      {
+        name: 'Gato 1',
+        image: 'assets/images/cat.jpg',
+        counter: 0
+      }, 
+    
+      {
+        name: 'Gato 2',
+        image: 'assets/images/cat2.jpg',
+        counter: 0
+      },
+    
+      {
+        name: 'Gato 3',
+        image: 'assets/images/cat3.jpg',
+        counter: 0
+      },
+    
+      {
+        name: 'Gato 4',
+        image: 'assets/images/cat4.jpg',
+        counter: 0
+      },
+    
+      {
+        name: 'Gato 5',
+        image: 'assets/images/cat5.jpg',
+        counter: 0
+      }
+    ];
+  },
+
+  getCatList: function() {
+    return this.catList;
+  }
+}
+
+  var octopus = {
+    init: function() {
+      model.init();
+
+      viewContent.init();    
+      
+      octopus.setSelected(octopus.getCatList()[0]);
+      
+      viewMenu.init();
     },
-  
-    {
-      name: 'Gato 3',
-      image: 'assets/images/cat3.jpg',
-      counter: 0
+
+    getCatList() {
+      return model.getCatList();
     },
-  
-    {
-      name: 'Gato 4',
-      image: 'assets/images/cat4.jpg',
-      counter: 0
+
+    setSelected: function(cat) {
+      this.selected = cat;
+      viewContent.render();
     },
-  
-    {
-      name: 'Gato 5',
-      image: 'assets/images/cat5.jpg',
-      counter: 0
+
+    getSelected: function() {
+      return this.selected;
+    },
+
+    addClickCounter: function() {
+      this.selected.counter++;
     }
-  ];
-  
-  var selected;
-  
-  var onCounterClick = function() {
-    var catCounter = document.getElementById('cat-counter');
-  
-    catCounter.innerText = ++selected.counter;
   }
-  
-  var catSelected = function(item) {
-    var catName = document.getElementById('cat-name');
-    var catImage = document.getElementById('cat-image');
-    var catCounter = document.getElementById('cat-counter');
-  
-    catName.innerText = item.name;
-    catImage.setAttribute('src', item.image);
-    catCounter.innerText = item.counter;
-  
-    selected = item;
-  
-    catImage.removeEventListener('click', onCounterClick);
-  
-    catImage.addEventListener('click', onCounterClick);
+
+var viewMenu = {
+  init: function() {
+    this.catListEl = document.getElementById('cat-list');
+    viewMenu.render();
+  },
+
+  render: function() {
+    var getNodeItem = function(item, index) {
+      var link = document.createElement('A');
+      link.setAttribute('href', '#');
+      link.setAttribute('class', 'collection-item');
+      
+      var textNode = document.createTextNode(item.name);
+      
+      link.appendChild(textNode);
+      
+      link.addEventListener('click', function() {
+        octopus.setSelected(item);
+      })
+
+      return link;
+    }
+
+    var list = octopus.getCatList();
+
+    for (var i = 0; i < list.length; i++) {
+      this.catListEl.appendChild(getNodeItem(list[i], i));
+    }
   }
-  
-  var getNodeItem = function(item, index) {
-    var link = document.createElement('A');
-    link.setAttribute('href', '#');
-    link.setAttribute('class', 'collection-item');
-  
-    var textNode = document.createTextNode(item.name);
-   
-    link.appendChild(textNode);
-  
-    link.addEventListener('click', function() {
-      catSelected(item);
-    })
-  
-    return link;
-  } 
-  
-  var catListEl = document.getElementById('cat-list');
-  
-  for (var i = 0; i < catList.length; i++) {
-    catListEl.appendChild(getNodeItem(catList[i], i));
+}
+
+var viewContent = {
+  init: function() {
+    this.catName = document.getElementById('cat-name');
+    this.catImage = document.getElementById('cat-image');
+    this.catCounter = document.getElementById('cat-counter');
+
+    this.catImage.addEventListener('click', function() {
+      octopus.addClickCounter();
+      viewContent.render();
+    });
+    
+    viewContent.render;
+  },
+
+  render: function() {
+    var item = octopus.getSelected();
+
+    this.catName.innerText = item.name;
+    this.catImage.setAttribute('src', item.image);
+    this.catCounter.innerText = item.counter;
   }
-  
-  catSelected(catList[0]);
+}
+
+octopus.init();
